4db003013dcb0edd09a168dd619315a4ef321b42
Oleksii Berliziev <oleksii.berliziev@engagepoint.com>
2015-10-15 17:12:28 +0300

    Temporary

--- js/Application.js
+++ js/Application.js
@@ -1,10 +1,11 @@
 define(function(require, exports, module){
     var Marionette = require("marionette"),
+        $ = require("jquery");
 
     application = new Marionette.Application();
 
     application.on("before:start", function(){
-        //
+        //$("#startDate").click =
     });
 
     module.exports = application;
