b13ae78d534e286d3c71a47cafd56af45f8cdfdc
unknown <andrii.polishchuk@ep-ua-apolishch.engagepoint.ua>
2015-10-15 18:51:44 +0300

    add collections

---
+++ js/collections/UserDaysMenuCollection.js
@@ -0,0 +1,13 @@
+define(function(require, exports, module){
+    var $ = require('jquery'),
+        _ = require('underscore'),
+        Backbone = require('backbone'),
+        UserDayMenuModel = require('UserDayMenuModel');
+
+
+    module.exports = Backbone.Collection.extend({
+        model: UserDayMenuModel,
+        url: '../deliveryapp/db/user.json'
+    });
+
+});
