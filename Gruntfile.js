9ff239879731137190b3a26fbd64bbb2baf8ccc8
Oleksii Berliziev <oleksii.berliziev@engagepoint.com>
2015-11-02 13:40:26 +0200

    package fixed

--- Gruntfile.js
+++ Gruntfile.js
@@ -94,8 +94,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  //grunt.loadNpmTasks('grunt-pngmin');
-  //grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('build', ['sass','concat','watch']);
+  grunt.registerTask('build', ['copy','sass','concat','watch']);
 };
