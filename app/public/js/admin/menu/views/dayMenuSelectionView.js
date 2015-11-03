044de9aa50ac98af2f279e77f0f2ac8c8770f720
Oleksii Berliziev <oleksii.berliziev@engagepoint.com>
2015-11-03 10:16:37 +0200

    Added field paymentStatus to orders in swagger

---
+++ app/public/js/admin/menu/views/dayMenuSelectionView.js
@@ -0,0 +1,19 @@
+define(function(require, exports, module){
+
+    var Marionette = require('marionette'),
+        UserDishCardView = require('userDishCardView'),
+        template = require('hbs!menu/views/templates/dayMenuSelectionView');
+
+    module.exports = Marionette.CompositeView.extend({
+
+        template: template,
+        childView: UserDishCardView,
+        childViewContainer: '#user-day-menu',
+        childEvents:{
+            'dishRemoved': 'dishClickedM'
+        },
+        dishClickedM: function (evt, model) {
+            this.collection.remove(model);
+        }
+    });
+});
---
+++ app/public/js/admin/menu/views/menuPreselectionView.js
@@ -0,0 +1,14 @@
+define(function(require, exports, module) {
+    var Marionette = require('marionette'),
+        template = require('hbs!menu/views/templates/menuPreselectionView');
+
+    module.exports = Marionette.LayoutView.extend({
+        template: template,
+        regions: {
+            'selectedUserMenu': '#selected-user-menu',
+            'dayMenu': '#day-menu'
+        }
+    });
+
+
+});
