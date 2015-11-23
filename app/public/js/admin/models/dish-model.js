6bd0a6a54e12eabe2e02491ae7c12511a78d22da
Zilot88 <zilot88@bigmir.net>
2015-11-23 16:03:11 +0200

    Repair category filter in user menu, and fix category template, from static in pattern, to dish model how constants .

--- app/public/js/admin/models/dish-model.js
+++ app/public/js/admin/models/dish-model.js
@@ -3,19 +3,21 @@ define(function(require, exports, module){
         _ = require('underscore'),
         baseUrl = require('baseUrl'),
         dishesResource = require('dishesResource'),
+        DishListCategory = require('dishListCategory'),
         Backbone = require('backbone');
 
 
     module.exports  = Backbone.Model.extend({
         urlRoot: baseUrl+dishesResource,
         idAttribute: '_id',
-        setPostUrl: function () {
+                setPostUrl: function () {
             this.url = baseUrl+dishesResource;
         },
         setPutUrl: function () {
             this.url = baseUrl+dishesResource+'/'+this.get('_id');
         },
         defaults: {
+            categoryList: DishListCategory()
         }
     });
 
