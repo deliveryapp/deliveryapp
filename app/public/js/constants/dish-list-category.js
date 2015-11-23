9f1e1022396db32bf9c48bae392a6f1b9d90cb7e
Zilot88 <zilot88@bigmir.net>
2015-11-23 16:29:39 +0200

    Fix for category filter

--- app/public/js/constants/dish-list-category.js
+++ app/public/js/constants/dish-list-category.js
@@ -33,6 +33,10 @@ define(function(require, exports, module){
             {
                 val: 'meatless meals',
                 name: 'постные блюда'
+            },
+            {
+                val: 'bread',
+                name: 'хлеб'
             }
         ]
     }
