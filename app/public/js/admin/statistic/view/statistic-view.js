b7bde2fd79c26971096dabf4ad793779b240eb3a
unknown <andrii.polishchuk@ep-ua-apolishch.engagepoint.ua>
2015-11-24 13:20:50 +0200

    fix statistic button

--- app/public/js/admin/statistic/view/statistic-view.js
+++ app/public/js/admin/statistic/view/statistic-view.js
@@ -11,30 +11,35 @@ define(function(require, exports, module) {
         },
         ui:{
             status: '.js-payment-status',
-            button: '.js-button-paid-status'
+            button: '.js-button-paid-status',
+            icon: '.js-button-icon'
         },
 
         onShow: function(){
             if (this.model.get('paymentStatus') === true){
                 this.ui.status.addClass('b-statistic-personal-info_paid');
-                this.ui.button.addClass('b-button_red');
+                this.ui.button.addClass('b-button-icon-text_red');
+                this.ui.icon.html('close');
             }
             else{
-                this.ui.button.addClass('b-button_green');
+                this.ui.button.addClass('b-button-icon-text_green');
+                this.ui.icon.html('done');
             }
         },
 
         paid: function(){
             this.trigger('payment:status:changed', this.model.get('_id'));
 
-            if (this.ui.button.hasClass('b-button_green')){
-                this.ui.button.removeClass('b-button_green');
-                this.ui.button.addClass('b-button_red');
+            if (this.ui.button.hasClass('b-button-icon-text_green')){
+                this.ui.button.removeClass('b-button-icon-text_green');
+                this.ui.button.addClass('b-button-icon-text_red');
+                this.ui.icon.html('close');
                 this.ui.status.addClass('b-statistic-personal-info_paid');
             }
             else{
-                this.ui.button.removeClass('b-button_red');
-                this.ui.button.addClass('b-button_green');
+                this.ui.button.removeClass('b-button-icon-text_red');
+                this.ui.button.addClass('b-button-icon-text_green');
+                this.ui.icon.html('done');
                 this.ui.status.removeClass('b-statistic-personal-info_paid');
             }
 
