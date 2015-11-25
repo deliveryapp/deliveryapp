20f490223145bdd7b69520a55241e4709711f1e0
Oleksii Berliziev <oleksii.berliziev@engagepoint.com>
2015-11-25 17:54:47 +0200

    admin menu notification and notification old view bug fixed

--- app/public/js/admin/menu/views/day-menu-selection-view.js
+++ app/public/js/admin/menu/views/day-menu-selection-view.js
@@ -1,6 +1,8 @@
 define(function(require, exports, module){
 
     var Marionette = require('marionette'),
+        Backbone = require ('backbone'),
+        BackboneRadio = require('backboneRadio'),
         AdminDishCardView = require('adminDishCardView'),
         AdminDishCardEmpty = require('adminDishCardEmpty'),
         template = require('hbs!menu/views/templates/day-menu-selection-view');
@@ -16,6 +18,60 @@ define(function(require, exports, module){
         },
         events: {
             'click .js-button-icon-save-day-menu': 'saveDayMenu'
+        },
+        ui: {
+            notification: '.js-menu-notification'
+        },
+        initialize: function(){
+            this.notifyScroll();
+        },
+        notifyScroll: function(){
+            $(window).scroll(function() {
+                var top = $(document).scrollTop();
+                if (top > 346) {$('.js-menu-notification').addClass('b-menu-notification_fixed');}
+                else {$('.js-menu-notification').removeClass('b-menu-notification_fixed');}
+            });
+        },
+        animateNotify: function(){
+            var opacity = 1;
+
+            function changeOpacity(){
+                opacity = opacity - 0.04;
+                $('.js-menu-notification').css('opacity',opacity);
+                if (opacity < 0){
+                    clearInterval(timer);
+                    $('.js-menu-notification').css('display','none');
+                }
+            }
+            var timer = setInterval(changeOpacity,20);
+        },
+        notifyParam: function(notification){
+            this.model = new Backbone.Model (notification);
+
+            this.render();
+            this.ui.notification.addClass('b-menu-notification_green').css('display','block');
+            if ($(document).scrollTop() > 346) $('.js-menu-notification').addClass('b-menu-notification_fixed');
+            setTimeout(this.animateNotify,5000);
+
+        },
+        onRender: function(){
+            this.userChannel = BackboneRadio.channel('user');
+            this.userChannel.on('notification:add', function(notification) {
+                    //debugger;
+                    console.log(notification);
+                    if (notification.type.toLowerCase() === 'put'){
+                        notification.type = 'Day menu successfully updated!';
+                        this.notifyParam(notification);
+                    }
+                    else if (notification.type === 'GET'){}
+
+                }.bind(this)
+
+            );
+
+        },
+        onDestroy: function () {
+            this.userChannel.reset();
         },
 
         saveDayMenu: function () {
